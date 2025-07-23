6be87695639bfc63ba2e9569443fd18be1d9f5f1
XnLemon <xianingawa@gmail.com>
2025-07-23 14:50:19 +0800

    多模态Test & 系统预设Prompt test

--- src/main/java/top/xnlemon/aicodehelper/ai/AiCodeHelper.java
+++ src/main/java/top/xnlemon/aicodehelper/ai/AiCodeHelper.java
@@ -1,6 +1,7 @@
 package top.xnlemon.aicodehelper.ai;
 
 import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.SystemMessage;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.chat.response.ChatResponse;
@@ -12,12 +13,26 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class AiCodeHelper {
 
+    private static final String SYSTEM_MESSAGE = "你是一名专业的AI代码助手，名称为Ne7ko。" +
+            "你的核心职能是帮助开发者高效完成编程相关任务，包括但不限于代码生成、调试、优化、解释和教学。" +
+            "你精通主流编程语言（Python/Java/JavaScript/C++等）和开发范式，严格遵守技术规范，同时具备教学能力。" ;
+        //Example System Message
+
 
     @Resource
     private ChatModel qwenchatModel;
-
+//    简单对话
     public String chat(String message){
+        SystemMessage systemMessage = SystemMessage.from(SYSTEM_MESSAGE);
         UserMessage userMessage = UserMessage.from(message);
+        ChatResponse chatResponse = qwenchatModel.chat(systemMessage, userMessage);
+        AiMessage aiMessage = chatResponse.aiMessage();
+        log.info("AI输出：" + aiMessage.toString());
+        return aiMessage.text();
+    }
+
+    //简单对话 - 用户消息自定义
+    public String chatwithmessage(UserMessage userMessage){
         ChatResponse chatResponse = qwenchatModel.chat(userMessage);
         AiMessage aiMessage = chatResponse.aiMessage();
         log.info("AI输出：" + aiMessage.toString());
--- src/test/java/top/xnlemon/aicodehelper/AiCodeHelperApplicationTests.java
+++ src/test/java/top/xnlemon/aicodehelper/AiCodeHelperApplicationTests.java
@@ -1,5 +1,8 @@
 package top.xnlemon.aicodehelper;
 
+import dev.langchain4j.data.message.ImageContent;
+import dev.langchain4j.data.message.TextContent;
+import dev.langchain4j.data.message.UserMessage;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,5 +18,15 @@ class AiCodeHelperApplicationTests {
     @Test
     void chat() {
         aiCodeHelper.chat("你好我是Nene7ko");
+    }
+
+    @Test
+    //need model supported Modalities
+    void chatwithmessage() {
+        UserMessage userMessage = UserMessage.from(
+                TextContent.from("请为Nene7ko描述此张图片"),
+                ImageContent.from("https://s2.loli.net/2025/07/10/VkCyflJNEvdKU3n.jpg")
+        );
+        aiCodeHelper.chatwithmessage(userMessage);
     }
 }
